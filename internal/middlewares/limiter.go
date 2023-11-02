32fd1b7be089537d111aa86bfa5b1708382af5e4
lucdoe <johndoe@example.com>
2023-11-02 10:47:15 +0100

    added rate limiter

--- internal/middlewares/limiter.go
+++ internal/middlewares/limiter.go
@@ -2,10 +2,24 @@ package middlewares
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"io"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucdoe/capstone_gateway/internal/app/databases"
+)
+
+const (
+	RateLimitKeyPrefix    = "rate-limit:"
+	HeaderRateLimit       = "X-RateLimit-Limit"
+	HeaderRateLimitRemain = "X-RateLimit-Remaining"
+	HeaderRateLimitReset  = "X-RateLimit-Reset"
+	MaxRequestsPerMinute  = 45
+	RateLimitWindow       = time.Minute
 )
 
 func LimitBodySize(c *gin.Context) {
@@ -26,4 +40,44 @@ func LimitBodySize(c *gin.Context) {
 	}
 
 	c.Next()
+}
+
+func RateLimit(c *gin.Context) {
+	count, err := increaseRateLimitCounter(c.ClientIP())
+	if err != nil {
+		c.Error(errors.New("Redis not started, could not process Rate-Limit."))
+		return
+	}
+
+	if count > MaxRequestsPerMinute {
+		c.JSON(http.StatusTooManyRequests, gin.H{"error": "Too many requests"})
+		c.Error(errors.New("Too many requests, please try again later.")).SetType(gin.ErrorTypePublic)
+		c.Abort()
+		return
+	}
+
+	setRateLimitHeaders(c, count)
+	c.Next()
+}
+
+func increaseRateLimitCounter(ip string) (int64, error) {
+	ctx := context.Background()
+	key := RateLimitKeyPrefix + ip
+
+	count, err := databases.RDB.Incr(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	if count == 1 {
+		databases.RDB.Expire(ctx, key, RateLimitWindow)
+	}
+
+	return count, nil
+}
+
+func setRateLimitHeaders(c *gin.Context, count int64) {
+	c.Header(HeaderRateLimit, strconv.Itoa(MaxRequestsPerMinute))
+	c.Header(HeaderRateLimitRemain, strconv.Itoa(MaxRequestsPerMinute-int(count)))
+	c.Header(HeaderRateLimitReset, strconv.Itoa(int(RateLimitWindow.Seconds())))
 }
--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -5,9 +5,9 @@ import "github.com/gin-gonic/gin"
 func InitilizeMiddlewares(r *gin.Engine) {
 	r.Use(gin.Recovery())
 
-	// add all middlewares here
 	r.Use(LogRequest)
 	r.Use(LimitBodySize)
 	r.Use(SecurityHeaders)
+	r.Use(RateLimit)
 	r.Use(CORS)
 }
