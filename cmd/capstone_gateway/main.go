013c55f1374398a73f3882d041a20a6e616a4bfc
lucdoe <johndoe@example.com>
2023-11-02 14:11:15 +0100

    accept config file for initilising routes

--- cmd/capstone_gateway/main.go
+++ cmd/capstone_gateway/main.go
@@ -2,18 +2,41 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/lucdoe/capstone_gateway/internal"
 	"github.com/lucdoe/capstone_gateway/internal/app"
 	"github.com/lucdoe/capstone_gateway/internal/app/databases"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
 	databases.InitializeRedis()
 
-	APIGatewayAPP, err := app.APIGatewayAPP()
+	config, err := LoadConfig("gateway_config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	APIGatewayAPP, err := app.APIGatewayAPP(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	APIGatewayAPP.Router.Run(":8080")
+}
+
+func LoadConfig(f string) (*internal.Config, error) {
+	data, err := os.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var config internal.Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/lucdoe/capstone_gateway/internal"
 	"github.com/lucdoe/capstone_gateway/internal/middlewares"
 )
 
@@ -9,11 +12,26 @@ type App struct {
 	Router *gin.Engine
 }
 
-func APIGatewayAPP() (*App, error) {
+func APIGatewayAPP(config *internal.Config) (*App, error) {
 	r := gin.New()
 
 	middlewares.InitilizeMiddlewares(r)
 
-	// loop over yaml to create routes
+	for serviceName, service := range config.Services {
+		for _, endpoint := range service.Endpoints {
+			switch endpoint.HTTPMethod {
+			case "GET":
+				r.GET(endpoint.Path, middlewares.ValidateJSONFields(endpoint.AllowedJSON))
+			case "POST":
+				r.POST(endpoint.Path, middlewares.ValidateJSONFields(endpoint.AllowedJSON))
+			case "PUT":
+				r.PUT(endpoint.Path, middlewares.ValidateJSONFields(endpoint.AllowedJSON))
+			case "PATCH":
+				r.PATCH(endpoint.Path, middlewares.ValidateJSONFields(endpoint.AllowedJSON))
+			}
+			fmt.Print(serviceName)
+		}
+	}
+
 	return &App{Router: r}, nil
 }
