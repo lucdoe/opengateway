88658b7b5059ba5d49846555e99768a6e31868a0
lucdoe <johndoe@example.com>
2023-11-02 14:08:34 +0100

    changed types pkg to internal

--- internal/types.go
+++ internal/types.go
@@ -1,1 +1,19 @@
-package app
+package internal
+
+type Endpoint struct {
+	Name        string   `yaml:"Name"`
+	HTTPMethod  string   `yaml:"HTTPMethod"`
+	Path        string   `yaml:"Path"`
+	AllowedJSON []string `yaml:"AllowedJSON"`
+}
+
+type Service struct {
+	PORT      int        `yaml:"PORT"`
+	URL       string     `yaml:"URL"`
+	SecretKey string     `yaml:"SecretKey"`
+	Endpoints []Endpoint `yaml:"Endpoints"`
+}
+
+type Config struct {
+	Services map[string]Service `yaml:"Services"`
+}
