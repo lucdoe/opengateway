f308e66e4e32829fe54f2f6b08a65754b8ec4ee7
lucdoe <lucdoe@users.noreply.github.com>
2024-02-05 19:02:56 +0100

    fixed requests with token

--- internal/middlewares/jwt.go
+++ internal/middlewares/jwt.go
@@ -12,32 +12,60 @@ import (
 func ValidateToken(shouldCheck bool, secretKey string) gin.HandlerFunc {
 	if shouldCheck {
 		return func(c *gin.Context) {
-			tokenString := strings.Split(c.GetHeader("Authorization"), " ")[1]
-
-			token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-				}
-
-				return []byte(secretKey), nil
-			})
-
-			if err != nil {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized, please provide valid token"})
-				c.Abort()
+			if !validateAuthHeader(c) {
 				return
 			}
 
-			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-				_ = claims
-				c.Next()
-			} else {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized, invalid token"})
-				c.Abort()
+			tokenString := getTokenFromHeader(c)
+			if !parseAndValidateToken(c, tokenString, secretKey) {
+				return
 			}
+
+			c.Next()
 		}
 	}
 	return func(c *gin.Context) {
 		c.Next()
 	}
+}
+
+func validateAuthHeader(c *gin.Context) bool {
+	authHeader := c.GetHeader("Authorization")
+	splitToken := strings.Split(authHeader, " ")
+
+	if len(splitToken) != 2 || splitToken[0] != "Bearer" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized, please provide valid token in authorization header with format: Bearer {token}"})
+		c.Abort()
+		return false
+	}
+	return true
+}
+
+func getTokenFromHeader(c *gin.Context) string {
+	authHeader := c.GetHeader("Authorization")
+	splitToken := strings.Split(authHeader, " ")
+	return splitToken[1]
+}
+
+func parseAndValidateToken(c *gin.Context, tokenString string, secretKey string) bool {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized, invalid token format"})
+		c.Abort()
+		return false
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok || !token.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized, invalid token"})
+		c.Abort()
+		return false
+	}
+
+	return true
 }
