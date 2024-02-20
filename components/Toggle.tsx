053b83126c56a601bcf304610dca04cb7295ac36
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-02-20 17:20:31 +0100

    Refactor Toggle component to accept label, onChange, and clicked props

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,24 +1,29 @@
 'use client'
 import {Switch} from '@headlessui/react'
-import {useState} from 'react'
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Toggle() {
-  const [enabled, setEnabled] = useState(false)
-
+export default function Toggle({
+  label,
+  onChange,
+  clicked,
+}: Readonly<{
+  label: string
+  onChange: (checked: boolean) => void
+  clicked: any
+}>) {
   return (
     <div className='mt-4'>
       <div>
         <label htmlFor='toggle' className='text-sm font-medium text-gray-900'>
-          Status: {enabled ? 'Active' : 'Disabled'}
+          {label}: {clicked ? 'Active' : 'Disabled'}
         </label>
         <div className='mt-2 '>
           <Switch
-            checked={enabled}
-            onChange={setEnabled}
+            checked={clicked}
+            onChange={(checked: boolean) => onChange(checked)}
             className='group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-full'>
             <span className='sr-only'>Use setting</span>
             <span
@@ -28,14 +33,14 @@ export default function Toggle() {
             <span
               aria-hidden='true'
               className={classNames(
-                enabled ? 'bg-green-400' : 'bg-red-400',
+                clicked ? 'bg-green-400' : 'bg-red-400',
                 'pointer-events-none absolute mx-auto h-4 w-9 rounded-full transition-colors duration-200 ease-in-out',
               )}
             />
             <span
               aria-hidden='true'
               className={classNames(
-                enabled ? 'translate-x-5' : 'translate-x-0',
+                clicked ? 'translate-x-5' : 'translate-x-0',
                 'pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out',
               )}
             />
