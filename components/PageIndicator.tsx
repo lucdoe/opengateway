2c67d609dce178bd3d9de3ca2f3425bbf7b4b911
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-02-20 15:51:11 +0100

    make page indicator clickable (Link)

--- components/PageIndicator.tsx
+++ components/PageIndicator.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function PageIndicator({
   page,
   subpage,
@@ -8,7 +10,9 @@ export default function PageIndicator({
   return (
     <div className='mb-8'>
       <span className='text-gray-500'>
-        <span className='text-gray-700'>{page}</span>{' '}
+        <Link href={'/' + page.toLowerCase()}>
+          <span className='text-gray-700'>{page}</span>
+        </Link>{' '}
         <span className='px-2'>{'>'}</span> {subpage}
       </span>
     </div>
