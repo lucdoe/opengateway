85b86b9baf06396e794f7a0d7cdf417666c9607a
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-02-21 16:31:36 +0100

    fetch single service and endpoint added

--- app/endpoints/[id]/page.tsx
+++ app/endpoints/[id]/page.tsx
@@ -1,4 +1,23 @@
+'use client'
+import {usePathname} from 'next/navigation'
+import {useEffect, useState} from 'react'
+
 export default function SingleEndpoint() {
+  const [service, setService] = useState(null)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `http://localhost:3001/api/services/${pathname.split('/').pop()}`,
+      )
+      const data = await res.json()
+      setService(data)
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div>
       <header>
@@ -9,6 +28,9 @@ export default function SingleEndpoint() {
       <p className='mt-2 text-sm text-gray-700'>
         Page to see all the details of a single endpoint.
       </p>
+      <div className='mt-4'>
+        <pre className='text-gray-800'>{JSON.stringify(service, null, 2)}</pre>
+      </div>
     </div>
   )
 }
