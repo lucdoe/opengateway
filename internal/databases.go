c5b565edf14c3c6a831f00ded90696ae46492af4
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-02-27 01:07:46 +0100

    Add database initialization and migration functions

---
+++ internal/databases.go
@@ -0,0 +1,54 @@
+package internal
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// https://redis.io/docs/connect/clients/go/
+var RDB *redis.Client
+
+func InitializeRedis() error {
+	RDB = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	_, err := RDB.Ping(context.Background()).Result()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func InitializePostgres() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=capstone_gateway port=5432 sslmode=disable TimeZone=Europe/Berlin"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func MigrateDB(db *gorm.DB) error {
+	err := db.AutoMigrate(
+		&Service{},
+		&Endpoint{},
+		&Middleware{},
+		&MiddlewareConfiguration{},
+		&Path{},
+		&Tag{},
+		&Protocol{},
+		&HTTPHeader{},
+		&HTTPMethod{},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
