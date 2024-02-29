b4af7058902229c9502a59bac07ef64679679442
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-02-29 14:38:00 +0100

    added helpers

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,18 +1,18 @@
-package internal
+package helpers
 
 import "github.com/gin-gonic/gin"
 
 // handleError simplifies error response handling
-func handleError(c *gin.Context, statusCode int, err error) {
+func HandleError(c *gin.Context, statusCode int, err error) {
 	c.JSON(statusCode, gin.H{"code": statusCode, "status": "error", "data": err.Error()})
 }
 
 // handleSuccess simplifies success response handling
-func handleSuccess(c *gin.Context, statusCode int, data interface{}) {
+func HandleSuccess(c *gin.Context, statusCode int, data interface{}) {
 	c.JSON(statusCode, gin.H{"code": statusCode, "status": "success", "data": data})
 }
 
-func setHeaders(c *gin.Context, headers map[string]string) {
+func SetHeaders(c *gin.Context, headers map[string]string) {
 	for header, value := range headers {
 		c.Header(header, value)
 	}
