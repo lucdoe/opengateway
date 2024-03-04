d6f2dc7d920d30b8391fe47c3d30995d47f743d7
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-03-04 22:38:05 +0100

    simplified body limit, add more tests for body size variations

--- internal/middleware/body_limit.go
+++ internal/middleware/body_limit.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"io"
 	"net/http"
 )
 
@@ -9,18 +8,6 @@ func BodyLimitHandler(limit int64) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			r.Body = http.MaxBytesReader(w, r.Body, limit)
-
-			buf := make([]byte, limit+1)
-			n, err := r.Body.Read(buf)
-			if err != nil && err != io.EOF {
-				http.Error(w, "Request body too large", http.StatusRequestEntityTooLarge)
-				return
-			}
-
-			if int64(n) == limit+1 {
-				http.Error(w, "Request body too large", http.StatusRequestEntityTooLarge)
-				return
-			}
 
 			next.ServeHTTP(w, r)
 		})
--- tests/internal/middleware/body_limit_test.go
+++ tests/internal/middleware/body_limit_test.go
@@ -2,6 +2,7 @@ package middleware_test
 
 import (
 	"bytes"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,23 +10,67 @@ import (
 	"github.com/lucdoe/opengateway/internal/middleware"
 )
 
-func TestBodyLimitHandler(t *testing.T) {
-	handler := middleware.BodyLimitHandler(10)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
-		body := buf.String()
+func dummyHandlerWithErrCheck(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
 
-		if len(body) > 10 {
-			t.Errorf("Expected body length <= 10, got %d", len(body))
+	if err != nil {
+		if err.Error() == "http: request body too large" {
+			http.Error(w, "Request body too large", http.StatusRequestEntityTooLarge)
+			return
 		}
-	}))
 
-	req := httptest.NewRequest("POST", "http://example.com/foo", bytes.NewBufferString("12345678901"))
-	w := httptest.NewRecorder()
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-	handler.ServeHTTP(w, req)
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
 
-	if w.Code != http.StatusRequestEntityTooLarge {
-		t.Errorf("Expected %d, got %d", http.StatusRequestEntityTooLarge, w.Code)
+func TestBodyLimitHandler(t *testing.T) {
+	limit10bytes := int64(10)
+
+	handler := middleware.BodyLimitHandler(limit10bytes)(http.HandlerFunc(dummyHandlerWithErrCheck))
+
+	textCases := []struct {
+		name         string
+		body         string
+		wantStatus   int
+		wantResponse string
+	}{
+		{
+			name:       "Request Body Exactly at the Limit",
+			body:       "1234567890",
+			wantStatus: http.StatusOK,
+		},
+		{
+			name:       "Request Body Below the Limit",
+			body:       "12345",
+			wantStatus: http.StatusOK,
+		},
+		{
+			name:       "Boundary Testing - Below the Limit",
+			body:       "123456789",
+			wantStatus: http.StatusOK,
+		},
+		{
+			name:       "Boundary Testing - Above the Limit",
+			body:       "12345678901",
+			wantStatus: http.StatusRequestEntityTooLarge,
+		},
+	}
+
+	for _, tt := range textCases {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest("POST", "http://example.com/foo", bytes.NewBufferString(tt.body))
+
+			w := httptest.NewRecorder()
+
+			handler.ServeHTTP(w, req)
+
+			if w.Code != tt.wantStatus {
+				t.Errorf("%s: expected status code %d, got %d", tt.name, tt.wantStatus, w.Code)
+			}
+		})
 	}
 }
