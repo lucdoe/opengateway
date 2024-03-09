10cab45671cda09c1e2e1e32500bb95458ce1993
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-03-09 14:15:24 +0100

    Refactor CORS middleware to improve readability and maintainability

--- internal/middleware/CORS.go
+++ internal/middleware/CORS.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"errors"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -19,11 +17,11 @@ type CORS struct {
 func CORSHandler(c CORS) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", c.AccessControlAllowOrigin)
-			w.Header().Set("Access-Control-Allow-Methods", c.AccessControlAllowMethods)
+			w.Header().Set("Access-Control-Allow-Origin", c.getAllowOrigin())
+			w.Header().Set("Access-Control-Allow-Methods", getValidMethods(c.AccessControlAllowMethods))
 			w.Header().Set("Access-Control-Allow-Headers", c.AccessControlAllowHeaders)
 			w.Header().Set("Access-Control-Expose-Headers", c.AccessControlExposeHeaders)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Credentials", boolToString(c.AccessControlAllowCredentials))
 			w.Header().Set("Access-Control-Max-Age", c.AccessControlMaxAge)
 
 			if r.Method == http.MethodOptions {
@@ -36,37 +34,37 @@ func CORSHandler(c CORS) func(http.Handler) http.Handler {
 	}
 }
 
-func (c *CORS) SetAccessControlAllowOrigin(origin string) {
-	if origin == "" {
-		origin = "*"
-		log.Println("CORS: Access-Control-Allow-Origin is not set. Defaulting to '*' (not recommended in production)")
+func (c *CORS) getAllowOrigin() string {
+	if c.AccessControlAllowOrigin == "" {
+		return "*"
 	}
-	c.AccessControlAllowOrigin = origin
+	return c.AccessControlAllowOrigin
 }
 
-func (c *CORS) SetAccessControlAllowMethods(methods []string) error {
+func getValidMethods(methods string) string {
 	validMethods := map[string]bool{
 		"GET": true, "HEAD": true, "POST": true, "PUT": true,
 		"DELETE": true, "CONNECT": true, "OPTIONS": true, "TRACE": true, "PATCH": true,
 	}
 
+	methodsSlice := strings.Split(methods, ",")
 	var allowedMethods []string
-	for _, method := range methods {
-		if _, exists := validMethods[method]; exists {
+	for _, method := range methodsSlice {
+		if validMethods[strings.ToUpper(method)] {
 			allowedMethods = append(allowedMethods, method)
-		} else {
-			return errors.New("Method " + method + " is not a valid HTTP method")
 		}
 	}
 
-	c.AccessControlAllowMethods = strings.Join(allowedMethods, ",")
-	return nil
-}
-
-func (c *CORS) SetAccessControllMaxAge(maxAge string) {
-	if maxAge == "" {
-		log.Println("CORS: Access-Control-Max-Age is not set. Defaulting to 0 (not recommended in production), which disables caching.")
+	if len(allowedMethods) == 0 {
+		return "GET"
 	}
-	c.AccessControlMaxAge = maxAge
+
+	return strings.Join(allowedMethods, ",")
 }
 
+func boolToString(b bool) string {
+	if b {
+		return "true"
+	}
+	return "false"
+}
