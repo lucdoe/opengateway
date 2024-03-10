c520a915371c9468f26a4f5866d83fd2fc3c4174
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-03-10 22:26:33 +0100

    wirering everything up in main

--- cmd/opengateway/main.go
+++ cmd/opengateway/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/lucdoe/opengateway/internal/config"
+	"github.com/lucdoe/opengateway/internal/handler"
+	"github.com/lucdoe/opengateway/internal/middleware"
+	"github.com/lucdoe/opengateway/internal/utils"
+)
+
 func main() {
+	fileReader := &config.OSFileReader{}
+	loader := config.NewLoader(fileReader)
+
+	cfg, err := loader.LoadConfigFromFile("config.yaml")
+	if err != nil {
+		fmt.Println("Failed to load config:", err)
+		return
+	}
+
+	mux := http.NewServeMux()
+	urlConstructor := utils.GatewayURLConstructor()
+	mwHandler := middleware.MiddlewareHandlerConstructor()
+	server := handler.APIGatewayServer()
+
+	handler.GatewayHandler(cfg, mux, urlConstructor, mwHandler, server)
 }
