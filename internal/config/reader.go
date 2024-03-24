b3d964cf90330ea58832715d934b269b48feacde
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-03-24 19:51:45 +0100

    Add FileReader interface and concreteFileReader implementation

--- internal/config/reader.go
+++ internal/config/reader.go
@@ -1,1 +1,21 @@
 package config
+
+type Reader interface {
+	ReadFile(name string) ([]byte, error)
+}
+
+type concreteFileReader struct {
+	reader FileReader
+}
+
+type FileReader interface {
+	ReadFile(name string) ([]byte, error)
+}
+
+func NewFileReader(reader FileReader) Reader {
+	return &concreteFileReader{reader: reader}
+}
+
+func (r *concreteFileReader) ReadFile(name string) ([]byte, error) {
+	return r.reader.ReadFile(name)
+}
