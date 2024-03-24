b2d76604c7d60aee2913ce9f3f027466643d1bdb
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-03-24 19:51:50 +0100

    Refactor Redis cache implementation

--- internal/data/cache.go
+++ internal/data/cache.go
@@ -10,26 +10,23 @@ type CacheClient interface {
 	Get(key string) (string, error)
 }
 
-// implements CacheClient
-type redisClient struct {
+type concreteRedisClient struct {
 	client RedisClient
 }
 
-// abstracts the go-redis client
 type RedisClient interface {
 	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
 	Get(ctx context.Context, key string) (string, error)
 }
 
-// when called: instanciates a redisClient
 func NewRedisCache(client RedisClient) CacheClient {
-	return &redisClient{client: client}
+	return &concreteRedisClient{client: client}
 }
 
-func (r *redisClient) Set(key string, value interface{}, expiration time.Duration) error {
+func (r *concreteRedisClient) Set(key string, value interface{}, expiration time.Duration) error {
 	return r.client.Set(context.Background(), key, value, expiration)
 }
 
-func (r *redisClient) Get(key string) (string, error) {
+func (r *concreteRedisClient) Get(key string) (string, error) {
 	return r.client.Get(context.Background(), key)
 }
