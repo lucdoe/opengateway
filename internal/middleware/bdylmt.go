22735868f13b0cd4fad01f08c0d56f2f80772bfa
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-04-01 18:11:30 +0200

    added body limiter

--- internal/middleware/bdylmt.go
+++ internal/middleware/bdylmt.go
@@ -1,1 +1,24 @@
 package middleware
+
+import (
+	"net/http"
+)
+
+type BodyLimiter interface {
+	LimitRequestBody(next http.Handler) http.Handler
+}
+
+type concreteBodyLimiter struct {
+	Limit int64
+}
+
+func NewBodyLimiter(limit int64) BodyLimiter {
+	return &concreteBodyLimiter{Limit: limit}
+}
+
+func (bl *concreteBodyLimiter) LimitRequestBody(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Body = http.MaxBytesReader(w, r.Body, bl.Limit)
+		next.ServeHTTP(w, r)
+	})
+}
