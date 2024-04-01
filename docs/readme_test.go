98bf784d9da0337d732aef86f39d67b51c6bf2e5
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-04-01 13:54:30 +0200

    added readme test

---
+++ docs/readme_test.go
@@ -0,0 +1,33 @@
+package docs_test
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestIfReadMeExists(t *testing.T) {
+	if _, err := os.Stat("./README.md"); os.IsNotExist(err) {
+		t.Errorf("README.md does not exist")
+		return
+	}
+
+	file, err := os.Open("./README.md")
+	if err != nil {
+		t.Errorf("README.md could not be opened")
+		return
+	}
+	defer file.Close()
+
+	buf := make([]byte, 512)
+	_, err = file.Read(buf)
+	if err != nil {
+		t.Errorf("README.md could not be read")
+		return
+	}
+
+	if !bytes.Contains(buf, []byte("# Fire API Gateway")) {
+		t.Errorf("h1 does not exist in README.md")
+		return
+	}
+}
