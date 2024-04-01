d5ad64e7549e8d361a4183a35781cad6c71ef886
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-04-01 18:25:54 +0200

    Add GZIP compression middleware

--- internal/middleware/compression.go
+++ internal/middleware/compression.go
@@ -1,1 +1,44 @@
 package middleware
+
+import (
+	"compress/gzip"
+	"net/http"
+	"strings"
+)
+
+type GZIPCompressor interface {
+	Compress(next http.Handler) http.Handler
+}
+
+type concreteGZIPCompressor struct{}
+
+func NewGZIPCompressor() GZIPCompressor {
+	return &concreteGZIPCompressor{}
+}
+
+func (c *concreteGZIPCompressor) Compress(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var hasGZIPCompressionHeader bool = strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
+
+		if hasGZIPCompressionHeader {
+			gw := gzip.NewWriter(w)
+			defer gw.Close()
+
+			w.Header().Set("Content-Encoding", "gzip")
+			wrappedWriter := &gzipResponseWriter{Writer: gw, ResponseWriter: w}
+
+			next.ServeHTTP(wrappedWriter, r)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writer *gzip.Writer
+}
+
+func (w *gzipResponseWriter) Write(data []byte) (int, error) {
+	return w.Writer.Write(data)
+}
