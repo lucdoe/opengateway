9d155455c7f11a58ca12918cb1786c9e3b98663e
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-05-10 14:15:14 +0200

    added server setup with mux

--- gateway/go.mod
+++ gateway/go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/yuin/gopher-lua v1.1.1 // indirect
--- gateway/internal/server/server.go
+++ gateway/internal/server/server.go
@@ -1,1 +1,46 @@
 package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/lucdoe/open-gateway/gateway/internal/config"
+	"github.com/lucdoe/open-gateway/gateway/internal/proxy"
+)
+
+type Server struct {
+	Router *mux.Router
+}
+
+func NewServer() *Server {
+	return &Server{
+		Router: mux.NewRouter(),
+	}
+}
+
+func (s *Server) SetupRoutes(cfg *config.Config) {
+	for serviceName, service := range cfg.Services {
+		for _, endpoint := range service.Endpoints {
+			targetURL := service.URL
+
+			handler := func(w http.ResponseWriter, r *http.Request) {
+				proxyService := proxy.NewProxyService()
+				err := proxyService.ReverseProxy(targetURL, w, r)
+				if err != nil {
+					http.Error(w, "Proxy error", http.StatusInternalServerError)
+					log.Printf("Error handling request to %s: %v", targetURL, err)
+				}
+			}
+
+			s.Router.HandleFunc(endpoint.Path, handler).Methods(endpoint.HTTPMethod)
+			log.Printf("Registered %s ->  %s", endpoint.HTTPMethod, targetURL+endpoint.Path)
+		}
+		log.Printf("Service %s setup completed", serviceName)
+	}
+}
+
+func (s *Server) Run() error {
+	log.Println("Starting server on http://localhost:4000")
+	return http.ListenAndServe(":4000", s.Router)
+}
