27013f73d6201a31ca599fd894ceec1d0cf488d6
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-05-10 14:14:47 +0200

    added main.go

--- gateway/cmd/gateway/main.go
+++ gateway/cmd/gateway/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+
+	c "github.com/lucdoe/open-gateway/gateway/internal/config"
+	"github.com/lucdoe/open-gateway/gateway/internal/server"
+)
+
+func main() {
+	p := c.NewParser("./cmd/gateway/config.yaml")
+
+	cfg, err := p.Parse()
+	if err != nil {
+		log.Fatalf("Failed to parse configuration: %v", err)
+	}
+
+	server := server.NewServer()
+
+	server.SetupRoutes(cfg)
+
+	err = server.Run()
+	if err != nil {
+		log.Fatalf("Failed to run the server: %v", err)
+	}
+}
