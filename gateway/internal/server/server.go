444b8004824823fcf5f2da9f661677d1251e148f
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-05-10 16:02:11 +0200

    add plugin manager to server to apply plugins to services/ endpoints

--- gateway/internal/server/server.go
+++ gateway/internal/server/server.go
@@ -6,37 +6,43 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/lucdoe/open-gateway/gateway/internal/config"
+	plugin "github.com/lucdoe/open-gateway/gateway/internal/plugins"
 	"github.com/lucdoe/open-gateway/gateway/internal/proxy"
 )
 
 type Server struct {
-	Router *mux.Router
+	Router  *mux.Router
+	Plugins *plugin.Manager
 }
 
 func NewServer() *Server {
 	return &Server{
-		Router: mux.NewRouter(),
+		Router:  mux.NewRouter(),
+		Plugins: plugin.NewManager(),
 	}
 }
 
 func (s *Server) SetupRoutes(cfg *config.Config) {
-	for serviceName, service := range cfg.Services {
+	for _, service := range cfg.Services {
+		serviceHandler := func(next http.Handler) http.Handler {
+			return s.Plugins.ApplyPlugins(service.Plugins, next)
+		}
+
 		for _, endpoint := range service.Endpoints {
 			targetURL := service.URL
-
 			handler := func(w http.ResponseWriter, r *http.Request) {
 				proxyService := proxy.NewProxyService()
 				err := proxyService.ReverseProxy(targetURL, w, r)
 				if err != nil {
 					http.Error(w, "Proxy error", http.StatusInternalServerError)
-					log.Printf("Error handling request to %s: %v", targetURL, err)
 				}
 			}
 
-			s.Router.HandleFunc(endpoint.Path, handler).Methods(endpoint.HTTPMethod)
-			log.Printf("Registered %s ->  %s", endpoint.HTTPMethod, targetURL+endpoint.Path)
+			endpointHandler := serviceHandler(http.HandlerFunc(handler))
+
+			wrappedHandler := s.Plugins.ApplyPlugins(endpoint.Plugins, endpointHandler)
+			s.Router.HandleFunc(endpoint.Path, wrappedHandler.ServeHTTP).Methods(endpoint.HTTPMethod)
 		}
-		log.Printf("Service %s setup completed", serviceName)
 	}
 }
 
