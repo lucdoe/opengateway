1ff42e9c4e41241b3d0d57ce747d299d3738db55
lucdoe <42179648+lucdoe@users.noreply.github.com>
2024-05-14 16:58:25 +0200

    run logger async

--- internal/server/middleware/logger.go
+++ internal/server/middleware/logger.go
@@ -1,17 +1,3 @@
-// Copyright 2024 lucdoe
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package middleware
 
 import (
@@ -34,7 +20,7 @@ func NewLoggingMiddleware(l logger.Logger) *LoggingMiddleware {
 func (lm *LoggingMiddleware) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		details := fmt.Sprintf("%s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
-		lm.Logger.Info("Request", details)
+		go lm.Logger.Info("Request", details)
 		next.ServeHTTP(w, r)
 	})
 }
